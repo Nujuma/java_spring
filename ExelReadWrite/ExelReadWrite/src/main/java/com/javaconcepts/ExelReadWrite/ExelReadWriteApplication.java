1754ebe87ececd571790909ae03445024e524231
Nujuma <nujumasyed@gmail.com>
2025-01-27 10:27:13 -0600

    initial commit

---
+++ ExelReadWrite/ExelReadWrite/src/main/java/com/javaconcepts/ExelReadWrite/ExelReadWriteApplication.java
@@ -0,0 +1,13 @@
+package com.javaconcepts.ExelReadWrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExelReadWriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExelReadWriteApplication.class, args);
+	}
+
+}
---
+++ ExelReadWrite/ExelReadWrite/src/test/java/com/javaconcepts/ExelReadWrite/ExelReadWriteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaconcepts.ExelReadWrite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExelReadWriteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
